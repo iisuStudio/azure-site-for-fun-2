3b90e47b6457142a39f02ec4f39fedf80cfa9066
steven <steven@pin2wall.com>
2019-05-08 16:36:59 +0800

    guess session error in middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
 
             //Packages
-            \PragmaRX\Tracker\Vendor\Laravel\Middlewares\Tracker::class,
+//            \PragmaRX\Tracker\Vendor\Laravel\Middlewares\Tracker::class,
         ],
 
         'api' => [
